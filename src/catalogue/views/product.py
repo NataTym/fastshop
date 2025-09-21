b56c2cd169cb7526c2bae751c9792e7d7c288985
Nataliia <timofeeva.natav@gmail.com>
2025-09-21 22:02:56 +0300

    changes in endpoints

--- src/catalogue/views/product.py
+++ src/catalogue/views/product.py
@@ -11,6 +11,7 @@ from fastapi import (
 )
 
 from src.catalogue.models.database import Product, AdditionalProducts, RecommendedProducts
+
 from src.catalogue.routes import (
     CatalogueRoutesPrefixes,
     ProductRoutesPrefixes,
@@ -36,6 +37,30 @@ async def product_list(product_service: Annotated[get_product_service, Depends()
         Response with list of products.
     """
     return await product_service.list()
+
+
+@router.get(
+    ProductRoutesPrefixes.additional,
+    status_code=status.HTTP_200_OK,
+    response_model=list[AdditionalProducts],
+)
+async def additional_list(additional_service: Annotated[get_additional_product_service, Depends()]) -> list[AdditionalProducts]:
+    return await additional_service.list()
+
+
+@router.get(
+    ProductRoutesPrefixes.recommended,
+    responses={
+        status.HTTP_200_OK: {'model': RecommendedProducts},
+        status.HTTP_404_NOT_FOUND: {'model': ErrorResponse},
+    },
+    status_code=status.HTTP_200_OK,
+    response_model=Union[RecommendedProducts, ErrorResponse],
+)
+async def recommended_products(
+    service: Annotated[get_recommended_product_service, Depends()]) -> Union[Response, ErrorResponse]:
+
+    return await service.list()
 
 
 @router.get(
@@ -67,46 +92,20 @@ async def product_detail(
     return response
 
 
-@router.get(
-    ProductRoutesPrefixes.additional,
-    responses={
-        status.HTTP_200_OK: {'model': AdditionalProducts},
-        status.HTTP_404_NOT_FOUND: {'model': ErrorResponse},
-    },
-    status_code=status.HTTP_200_OK,
-    response_model=Union[AdditionalProducts, ErrorResponse],
-)
-async def additional_products(
-    service: Annotated[get_additional_product_service, Depends()]) -> List[Product]:
-
-    return service.list()
-
-
-@router.get(
-    ProductRoutesPrefixes.recommended,
-    responses={
-        status.HTTP_200_OK: {'model': RecommendedProducts},
-        status.HTTP_404_NOT_FOUND: {'model': ErrorResponse},
-    },
-    status_code=status.HTTP_200_OK,
-    response_model=Union[RecommendedProducts, ErrorResponse],
-)
-async def recommended_products(
-    service: Annotated[get_recommended_product_service, Depends()]) -> Union[Response, ErrorResponse]:
-
-    return service.list()
-
-
-
 @router.post(
-    "/products/{product_id}/additional/{additional_id}",
+    ProductRoutesPrefixes.additional,
     response_model=AdditionalProducts,
     status_code=status.HTTP_201_CREATED,
 )
 async def add_additional_product(
-    product_id: int,
-    additional_id: int,
-    service: Annotated[get_additional_product_service, Depends()]
-):
-    obj = AdditionalProducts(primary_id=product_id, additional_id=additional_id)
-    return await service.create(obj)
+    data: AdditionalProducts,
+    service: Annotated[get_additional_product_service, Depends()]):
+    return await service.create(data)
+
+
+
+
+
+
+
+
